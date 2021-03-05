89a142875344e0cb391e0f04db096a92dd5c573a
stanajin <nimbalkarsachin@outlook.com>
2021-03-05 19:02:39 +0000

    First commit

---
+++ myJava/src/main/java/myJava/SwitchExample.java
@@ -0,0 +1,22 @@
+package myJava;
+
+public class SwitchExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	int number = 40;
+	
+	switch (number) {
+	case 10: System.out.println("10");
+	break;
+	case 20: System.out.println("20");
+	break;
+	case 30: System.out.println("30");
+	break;
+	default: System.out.println("none of the numbers");
+	}
+
+	}
+
+}
